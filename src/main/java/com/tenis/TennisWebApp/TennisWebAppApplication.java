98c3b36c8b3cb030724f6fb64263b4d3a1e2b1b2
Shark <dockalik39@gmail.com>
2020-04-11 17:18:25 +0200

    Spring Initializr

---
+++ src/main/java/com/tenis/TennisWebApp/TennisWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.tenis.TennisWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisWebAppApplication.class, args);
+	}
+
+}
